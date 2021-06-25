f8d38d923ed483febe737ef1c46c4abbc98ba923
albertojcr <albertojcr96@gmail.com>
2021-06-25 16:18:45 +0200

    Change for use MySqlPostRepository

--- src/Entrypoint/Controllers/Post/GetAllPostController.php
+++ src/Entrypoint/Controllers/Post/GetAllPostController.php
@@ -2,7 +2,8 @@
 
 namespace IESLaCierva\Entrypoint\Controllers\Post;
 
-use IESLaCierva\Application\Post\GetAllPost\GetAllPost;
+use IESLaCierva\Application\Post\GetAllPost\GetAllPostService;
+use IESLaCierva\Infrastructure\Database\MySqlPostRepository;
 use IESLaCierva\Infrastructure\Files\JsonPostRepository;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -10,9 +11,15 @@ use Symfony\Component\HttpFoundation\Response;
 
 class GetAllPostController
 {
+    private GetAllPostService $getAllPostService;
+
+    public function __construct() {
+        $this->getAllPostService = new GetAllPostService(new MySqlPostRepository());
+    }
+
     public function execute(Request $request): Response
     {
-        $service = new GetAllPost(new JsonPostRepository());
-        return new JsonResponse($service->execute());
+        $posts = $this->getAllPostService->execute();
+        return new JsonResponse($posts);
     }
 }
