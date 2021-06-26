885f407fbe633d8933ee73477e783a85a7b88cad
albertojcr <albertojcr96@gmail.com>
2021-06-26 20:13:56 +0200

    Change for use MySqlPostRepository

--- src/Entrypoint/Controllers/Post/GetPostByIdController.php
+++ src/Entrypoint/Controllers/Post/GetPostByIdController.php
@@ -3,6 +3,7 @@
 namespace IESLaCierva\Entrypoint\Controllers\Post;
 
 use IESLaCierva\Application\Post\GetPostById\GetPostByIdService;
+use IESLaCierva\Infrastructure\Database\MySqlPostRepository;
 use IESLaCierva\Infrastructure\Files\JsonPostRepository;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -12,7 +13,7 @@ class GetPostByIdController
 {
     public function execute(Request $request): Response
     {
-        $service = new GetPostByIdService(new JsonPostRepository());
+        $service = new GetPostByIdService(new MySqlPostRepository());
         return new JsonResponse($service->execute($request->get('postId')));
     }
 }
