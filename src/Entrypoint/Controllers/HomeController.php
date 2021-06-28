2bf1a445cedb58fc1c42a933d27f9c066037663d
albertojcr <albertojcr96@gmail.com>
2021-06-28 12:49:09 +0200

    Change index.html

--- src/Entrypoint/Controllers/HomeController.php
+++ src/Entrypoint/Controllers/HomeController.php
@@ -10,7 +10,7 @@ class HomeController
     {
         $response = new Response();
         ob_start();
-        require_once __DIR__.'/../../../public/index2.html';
+        require_once __DIR__.'/../../../public/index.html';
         $response->setContent(ob_get_clean());
         return $response;
     }
