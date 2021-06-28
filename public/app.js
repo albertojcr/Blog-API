0e98164d11effa9c6344d7f3e48a77c89f26a7e5
albertojcr <albertojcr96@gmail.com>
2021-06-28 13:38:24 +0200

    Edit btns

--- public/app.js
+++ public/app.js
@@ -8,10 +8,10 @@ getAllPost(renderPostTable);
 getAllUsers(renderUserTable);
 
 let newPostButton = document.getElementById('new-post');
-newPostButton.addEventListener('click', showNewPostModal)
+newPostButton.addEventListener('click', showNewPostModal);
 
 let newUserButton = document.getElementById('user-post');
-newUserButton.addEventListener('click', showNewUserModal)
+newUserButton.addEventListener('click', showNewUserModal);
 
 
 function getSessionInfo(callback) {
@@ -42,7 +42,7 @@ function renderNavbar() {
     let userElement = document.getElementById('user-info');
     userElement.innerHTML = `
                     <li class="nav-item rounded m-lg">
-                    <a class="nav-link active" aria-current="page" href="#">
+                    <a class="nav-link active" aria-current="page" href="/home">
                         <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-file-earmark-post" viewBox="0 0 16 16">
                             <path d="M14 4.5V14a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h5.5L14 4.5zm-3 0A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V4.5h-2z"/>
                             <path d="M4 6.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-.5.5h-7a.5.5 0 0 1-.5-.5v-7zm0-3a.5.5 0 0 1 .5-.5H7a.5.5 0 0 1 0 1H4.5a.5.5 0 0 1-.5-.5z"/>
@@ -51,7 +51,7 @@ function renderNavbar() {
                     </a>
                 </li>
 
-                <li class="nav-item rounded m-lg">
+                <li class="nav-item rounded m-lg" id="register" data-bs-toggle="modal" data-bs-target="#post-modal">
                     <a class="nav-link" href="#">
                         <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-at" viewBox="0 0 16 16">
                             <path d="M13.106 7.222c0-2.967-2.249-5.032-5.482-5.032-3.35 0-5.646 2.318-5.646 5.702 0 3.493 2.235 5.708 5.762 5.708.862 0 1.689-.123 2.304-.335v-.862c-.43.199-1.354.328-2.29.328-2.926 0-4.813-1.88-4.813-4.798 0-2.844 1.921-4.881 4.594-4.881 2.735 0 4.608 1.688 4.608 4.156 0 1.682-.554 2.769-1.416 2.769-.492 0-.772-.28-.772-.76V5.206H8.923v.834h-.11c-.266-.595-.881-.964-1.6-.964-1.4 0-2.378 1.162-2.378 2.823 0 1.737.957 2.906 2.379 2.906.8 0 1.415-.39 1.709-1.087h.11c.081.67.703 1.148 1.503 1.148 1.572 0 2.57-1.415 2.57-3.643zm-7.177.704c0-1.197.54-1.907 1.456-1.907.93 0 1.524.738 1.524 1.907S8.308 9.84 7.371 9.84c-.895 0-1.442-.725-1.442-1.914z"/>
@@ -68,6 +68,8 @@ function renderNavbar() {
                     </a>
                 </li>
     `;
+    let registerButton = document.getElementById('register');
+    registerButton.addEventListener('click', showNewUserModal);
 }
 
 function createLoginButton()
