cc824bed7458b01ffd348344714e118664b76639
albertojcr <albertojcr96@gmail.com>
2021-06-29 18:25:20 +0200

    Add new post button to navbar

--- public/app.js
+++ public/app.js
@@ -45,6 +45,7 @@ function renderNavbarButtons(session) {
         userElement.appendChild(createRegisterButton());
         userElement.appendChild(createLoginButton());
     } else {
+        userElement.appendChild(createNewPostButton());
         if (session.role === 'Administrador') {
             userElement.appendChild(createMyAccountButton());
             var dropdownMenu = document.getElementById('dropdown-menu');
@@ -101,6 +102,23 @@ function createLoginButton() {
                         Login
                     </a>`;
     button.addEventListener('click', showLoginModal);
+
+    return button;
+}
+
+function createNewPostButton() {
+    let button = document.createElement('li');
+    button.classList.add('nav-item', 'rounded', 'm-lg');
+    //button.setAttribute('id', 'register');
+    button.setAttribute('data-bs-toggle', "modal");
+    button.setAttribute('data-bs-target', "#post-modal");
+    button.innerHTML = `<a class="nav-link" href="#">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
+                              <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/>
+                            </svg>
+                            New post
+                        </a>`;
+    button.addEventListener('click', showNewPostModal);
 
     return button;
 }
